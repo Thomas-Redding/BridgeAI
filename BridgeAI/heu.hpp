1496348e8c1fc4e8b8f9569f5eb050fcf0400038
KurtGodel <mtredding@yahoo.com>
2015-12-27 17:29:25 -0500

    foo

--- BridgeAI/heu.hpp
+++ BridgeAI/heu.hpp
@@ -14,11 +14,16 @@
 
 /* heuristics */
 namespace heu {
+	// Tend to play the highest or lowest card in a suit
 	std::pair<std::vector<Card>, std::vector<double>> playExtremes(const History& history, const Hand& hand) {
 		std::pair<std::vector<Card>, std::vector<double>> rtn;
-		if(hand.cards[0].size() > 0) {
-			rtn.push_back(hands.cards[0][0]);
-			rtn.push_back(hands.cards[0][0]);
+		for (int i=0; i<hand.numberOfSuits; i++) {
+			if(hand.cards[i].size() >= 2) {
+				rtn.first.push_back(hand.cards[i][0]);
+				rtn.second.push_back(1);
+				rtn.first.push_back(hand.cards[i][1]);
+				rtn.second.push_back(1);
+			}
 		}
 		return rtn;
 	}
--- BridgeAI/main.cpp
+++ BridgeAI/main.cpp
@@ -7,6 +7,7 @@
 //
 
 #include <iostream>
+#include "heu.hpp"
 
 int main(int argc, const char * argv[]) {
 	// insert code here...
