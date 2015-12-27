3a5102900fa52e5c4ed56926e556cbfa1d8a0d81
KurtGodel <mtredding@yahoo.com>
2015-12-27 13:03:25 -0500

    first commit

---
+++ BridgeAI/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BridgeAI
+//
+//  Created by Thomas Redding on 12/27/15.
+//  Copyright © 2015 Thomas Redding. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
